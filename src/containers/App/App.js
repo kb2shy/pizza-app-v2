3cc6a275ee8e4a3e587eaca4343f48c510fb413a
Chhaian Pin <chhaianpin@hotmail.com>
2020-04-03 09:15:46 -0700

    created new container folder to hold App, CreatePizza, AddToppings, and Home components.

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 
+import AddToppings from '../AddToppings/AddToppings';
+import CreatePizza from '../CreatePizza/CreatePizza';
+import Home from '../Home/Home';
+
 class App extends Component {
     render() {
         return(
